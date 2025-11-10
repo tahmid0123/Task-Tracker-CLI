6f3d52b791e87cdb743b4747e9b4720c130f27b1
tahmid0123 <tahmidhasan42@yahoo.com>
2025-11-11 01:53:56 +0600

    Add proess task functionality

--- script.js
+++ script.js
@@ -2,25 +2,50 @@ const addTaskButton = document.getElementById('add-task');
 const taskContainer = document.querySelector('.task-container');
 const taskListUl = document.querySelector('.task-list')
 const errorMsg = document.querySelector('.error-msg')
+const taskInProgress = document.querySelector('.task-in-progress')
 
 addTaskButton.addEventListener('click', () => {
 
     let taskInputValue = document.getElementById('task').value;
 
+        
    
     if( taskInputValue != '' ){
+        let threeDigitsId = () => Math.floor(Math.random() * 900);
+        let randomId = threeDigitsId();
+
         let liEle = document.createElement('li');
         liEle.textContent = taskInputValue;
+        liEle.setAttribute('data-id',`${randomId}`);
         taskListUl.appendChild(liEle);
+
         document.getElementById('task').value = '';
+
         let progressBtn = document.createElement('button');
+        progressBtn.setAttribute('data-id', `${randomId}`)
         progressBtn.textContent = "Move to Progress";
-        taskListUl.appendChild(progressBtn)
-        console.log(progressBtn);
+        liEle.appendChild(progressBtn);
+
+        // empty the error message text
+        errorMsg.textContent = " "
+
     }else{
         errorMsg.textContent = "Please write your task first!"
     }
     
+});
+
+document.querySelector('.task-list').addEventListener('click', (e) => {
+    if(e.target.tagName === "BUTTON"){
+       const btnId = e.target.dataset.id;
+        const parent = e.target.parentElement;
+        const parentId = parent.getAttribute('data-id');
+        if(btnId === parentId){
+            e.target.textContent = "mark as done"
+            taskInProgress.appendChild(parent)
+        }
+        console.log(parentId);
+    }
     
 })
 
