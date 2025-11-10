40bcdfe1f99b0b59e62e1af84bce7a78eb01d33a
tahmid0123 <tahmidhasan42@yahoo.com>
2025-11-10 16:35:55 +0600

    Add the add-task functionality

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const addTaskButton = document.getElementById('add-task');
 const taskContainer = document.querySelector('.task-container');
+const taskListUl = document.querySelector('.task-list')
+const taskInputValue = document.getElementById('task');
 
-console.log(addTaskButton, taskContainer);
+addTaskButton.addEventListener('click', () => {
+   
+    const taskInputValue = document.getElementById('task').value;
+    if( taskInputValue !== '' ){
+        let liEle = document.createElement('li');
+        liEle.textContent = taskInputValue;
+        taskListUl.appendChild(liEle);
+        console.log(liEle);
+    }else{
+        console.log('Please write your task first!');
+    }
+    
+})
+
+
+
+console.log(taskContainer);
